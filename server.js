7ef2e1d9d799b3af0f33764bbfc40c3b7d8b4dd4
User <user@Users-MacBook-Air.local>
2024-02-05 20:02:40 -0500

    finished adding functionality for adding comments to a blog post

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get("/", async (req, res) => {
 app.get("/blogs/:id", async (req, res) => {
   console.log(req.session);
   const { id } = req.params;
-
+  const { user_id } = req.session;
   let blogPost = await BlogPosts.findAll({
     where: { id: id },
     raw: true,
@@ -72,7 +72,7 @@ app.get("/blogs/:id", async (req, res) => {
   });
 
   const comments = await Comments.findAll({
-    where: { userId: commentUserId },
+    where: { blogPostId: id },
     raw: true,
     include: [Users],
   });
@@ -82,7 +82,45 @@ app.get("/blogs/:id", async (req, res) => {
   return res.render("singleBlog", { blogPost, username, comments, user });
 });
 
+app.post("/blogs/:id/comment", async (req, res) => {
+  if (!req.session.loggedIn) {
+    return res.redirect("/login");
+  }
+
+  const { user_id } = req.session;
+  // console.log(req.session);
+  const { id } = req.params;
+  const { content } = req.body;
+
+  console.log(id, content);
+
+  let [blogPost] = await BlogPosts.findAll({
+    where: { id: id },
+    raw: true,
+    include: [Comments, Users],
+  });
+
+  const blogPostUser = blogPost.userId;
+
+  const [user] = await Users.findAll({
+    where: { id: user_id },
+    raw: true,
+  });
+  // console.log(blogPostUser);
+
+  let comment = await Comments.create({
+    content,
+    userId: user.id,
+    blogPostId: id,
+  });
+  console.log(comment);
+  res.redirect(`/blogs/${id}`);
+});
+
 app.get("/blogs/:id/comment", async (req, res) => {
+  if (!req.session.loggedIn) {
+    return res.redirect("/login");
+  }
   // console.log(req.session);
   const { id } = req.params;
 
@@ -150,31 +188,16 @@ app.post("/dashboard/newpost", async (req, res) => {
 
 app.get("/dashboard/edit/:id", async (req, res) => {
   const { id: blogPost_id } = req.params;
-  const { user_id: id } = req.session;
 
   const [blogPost] = await BlogPosts.findAll({
     where: { id: blogPost_id },
     raw: true,
   });
-
   return res.render("editPage", { blogPost });
 });
 
 app.post("/dashboard/edit/:id", async (req, res) => {
   const { id: blogPost_id } = req.params;
-  // const { user_id: id } = req.session;
-  // const { title, content } = req.body;
-  const [blogPost] = await BlogPosts.findAll({ where: { id: blogPost_id } });
-  // // console.log(blogPost);
-  // const [user] = await Users.findAll({
-  //   where: { id: blogPost.userId },
-  //   raw: true,
-  // });
-  // // console.log(user);
-
-  // // console.log(req.)
-console.log(blogPost_id)
-
   await BlogPosts.update(req.body, { where: { id: blogPost_id } });
   return res.redirect(`/dashboard`);
 });
@@ -190,10 +213,10 @@ app.post("/login", async (req, res) => {
 
   try {
     let userData = await Users.findOne({ where: { username }, raw: true });
-    // let validPassword = bcrypt.compare(userData.password);
 
     if (userData) {
-      validPassword = userData.password === password;
+      // validPassword = password === userData.password;
+      validPassword = await bcrypt.compare(password, userData.password);
     }
     // console.log(userData)
 
@@ -201,10 +224,6 @@ app.post("/login", async (req, res) => {
       if (!userData) {
         errMessages.push("No user with that username exists");
       }
-
-      // if (!validPassword) {
-      //   errMessages.push("No user with that password exists");
-      // }
 
       if (userData && !validPassword) {
         errMessages.push("Incorrect Password");
@@ -215,13 +234,15 @@ app.post("/login", async (req, res) => {
     }
 
     console.log(userData, validPassword);
+
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.username = userData.username;
       req.session.loggedIn = true;
     });
-    console.log("succesfully logged in");
-    res.render("login");
+
+    res.render("index");
+    // console.log(req.session)
   } catch (err) {
     console.log(err);
     res.status(404).redirect("/login");
